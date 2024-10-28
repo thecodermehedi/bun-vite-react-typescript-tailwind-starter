39f618afd98d978e38d4525b790d6fc497876efe
Mehedi Hasan <iammehedihasan@protonmail.com>
2024-10-28 09:21:22 +0600

    initialized

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+const App = () => {
+ return (
+  <main className="bg-gray-100 text-gray-900 flex flex-col items-center justify-center min-h-screen">
+   <div className="flex justify-center items-center p-4 gap-16 font-bold">
+    <div className="flex flex-col justify-center items-center size-26 gap-3">
+     <img src="https://skillicons.dev/icons?i=bun&theme=light" alt="bun-logo" />
+     <p>Bun</p>
+    </div>
+    <div className="flex flex-col justify-center items-center size-26 gap-3">
+     <img src="https://skillicons.dev/icons?i=vite&theme=light" alt="vite-logo" />
+     <p>Vite</p>
+    </div>
+    <div className="flex flex-col justify-center items-center size-26 gap-3">
+     <img src="https://skillicons.dev/icons?i=react&theme=light" alt="react-logo" />
+     <p>React</p>
+    </div>
+    <div className="flex flex-col justify-center items-center size-26 gap-3">
+     <img src="https://skillicons.dev/icons?i=ts&theme=light" alt="typescript-logo" />
+     <p>Typescript</p>
+    </div>
+    <div className="flex flex-col justify-center items-center size-26 gap-3">
+     <img src="https://skillicons.dev/icons?i=tailwind&theme=light" alt="tailwind-logos" />
+     <p>TailwindCSS</p>
+    </div>
+   </div>
+  </main>
+ );
+};
+
+export default App;
